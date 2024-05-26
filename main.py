544dd004eddb595fe1cba15c07aa4ec8b3b90478
NikEr2024 <n.erdyneev2000@gmail.com>
2024-05-26 16:21:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+my_string = input()
+print('Длина символов введённого текста: ', input(len(my_string)))
+
+print('Вверхний регистр:   ', my_string.upper())
+print("Нижний Регистр:   ", my_string.lower())
+print("Без пробелов:   ", my_string.replace(' ', ''))
+print("Первый символ:   ", my_string[:1])
+print("Последний символ:   ",my_string[-1])
